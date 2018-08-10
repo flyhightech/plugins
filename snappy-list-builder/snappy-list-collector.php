7555a5fc48a71fb440cff4cc6d1c77a75a1ea0e0
Bernard Huff <bernard.huff008@gmail.com>
2018-08-10 03:54:51 -0500

    Spaces

---
+++ snappy-list-builder/snappy-list-collector.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+Plugin Name: Snappy List Collector
+Plugin URI: http://www.flyhightech.net
+Description: One of the best plugins for collection visitors email addresses.
+version: 2.0
+Author: Flyhightech.LLC
+Author URI: https://www.flyhightech.net
+License: GPL2
+
+*/
+
+
+
+
+
+
+?>
